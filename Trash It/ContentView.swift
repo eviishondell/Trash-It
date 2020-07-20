82fef4d7d81e7b3163d56904c77783830a3270cc
eviishondell <elayne297@gmail.com>
2020-07-20 13:08:05 -0400

    First Commit
    Test

---
+++ Trash It/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trash It
+//
+//  Created by Dawn Layne on 7/20/20.
+//  Copyright © 2020 Evolone Layne. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("TRASH IT")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
